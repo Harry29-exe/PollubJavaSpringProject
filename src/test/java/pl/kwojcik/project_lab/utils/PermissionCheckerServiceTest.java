fa0d624eafe7f0dc9d172209c03c51bef56f869a
Kamil Wójcik <wojcikakamil@gmail.com>
2024-10-12 11:12:10 +0200

    added last 4 unit tests

--- src/test/java/pl/kwojcik/project_lab/utils/PermissionCheckerServiceTest.java
+++ src/test/java/pl/kwojcik/project_lab/utils/PermissionCheckerServiceTest.java
@@ -1,5 +1,6 @@
 package pl.kwojcik.project_lab.utils;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -15,12 +16,7 @@ class PermissionCheckerServiceTest {
 
     @Test
     void should_allow_for_method_execution() {
-        var ctx = new SecurityContextImpl();
-
-        ctx.setAuthentication(UsernamePasswordAuthenticationToken.authenticated(
-                "admin", "admin",
-                List.of(new SimpleGrantedAuthority(AppPermission.ORDER_VIEW.getRole()))));
-        SecurityContextHolder.setContext(ctx);
+        setPermission(List.of(AppPermission.ORDER_VIEW));
 
         var service = new PermissionCheckerService();
         service.checkPermissionsAndExecute(AppPermission.ORDER_VIEW, () -> {
@@ -30,6 +26,82 @@ class PermissionCheckerServiceTest {
 
         //then
         assert counter.get() == 1;
+    }
+
+    @Test
+    void should_allow_for_method_execution_while_having_multiple_permissions() {
+        setPermission(List.of(AppPermission.ORDER_VIEW, AppPermission.ORDER_MODIFY));
+
+        var service = new PermissionCheckerService();
+        service.checkPermissionsAndExecute(AppPermission.ORDER_VIEW, () -> {
+            counter.incrementAndGet();
+            return null;
+        });
+
+        //then
+        assert counter.get() == 1;
+    }
+
+    @Test
+    void should_not_allow_while_having_none_roles() {
+        setPermission(List.of());
+
+        var service = new PermissionCheckerService();
+
+        //then
+        Assertions.assertThrows(Exception.class, () -> {
+            //when
+            service.checkPermissionsAndExecute(AppPermission.ORDER_VIEW, () -> {
+                counter.incrementAndGet();
+                return null;
+            });
+        });
+    }
+
+    @Test
+    void should_not_allow_while_having_one_different_role() {
+        setPermission(List.of(AppPermission.ORDER_MODIFY));
+
+        var service = new PermissionCheckerService();
+
+        //then
+        Assertions.assertThrows(Exception.class, () -> {
+            //when
+            service.checkPermissionsAndExecute(AppPermission.ORDER_VIEW, () -> {
+                counter.incrementAndGet();
+                return null;
+            });
+        });
+    }
+
+    @Test
+    void should_not_allow_while_having_multiple_different_role() {
+        setPermission(List.of(AppPermission.ORDER_MODIFY, AppPermission.ADMIN_USER_CREATE));
+
+        var service = new PermissionCheckerService();
+
+        //then
+        Assertions.assertThrows(Exception.class, () -> {
+            //when
+            service.checkPermissionsAndExecute(AppPermission.ORDER_VIEW, () -> {
+                counter.incrementAndGet();
+                return null;
+            });
+        });
+    }
+
+    private static void setPermission(List<AppPermission> permissions) {
+        var ctx = new SecurityContextImpl();
+
+        ctx.setAuthentication(UsernamePasswordAuthenticationToken.authenticated(
+                "admin",
+                "admin",
+                permissions.stream()
+                        .map(p ->
+                                new SimpleGrantedAuthority(p.getRole()))
+                        .toList())
+        );
+        SecurityContextHolder.setContext(ctx);
     }
 
 }
